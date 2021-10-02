285ea93689acae87e5d8264afb1a7258d436dbfb
Rodrigo <rcuenca446@gmail.com>
2021-10-02 07:09:40 -0500

    feat: upload document view

--- app/src/main/java/com/example/studydesk/MainActivity.kt
+++ app/src/main/java/com/example/studydesk/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.studydesk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.upload_documents)
+
+        val button = findViewById<Button>(R.id.upload_button);
+
+
+        button.setOnClickListener {
+
+            val toast = Toast.makeText(this, "Documento creado correctamente", Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.TOP,0,0);
+            toast.show();
+        }
     }
 }
