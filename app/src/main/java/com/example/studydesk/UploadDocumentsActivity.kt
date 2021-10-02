8c0cbdbbaedac8fed851cdee1546978e88f97a02
CarlosIzarra09 <eduizarra2@hotmail.com>
2021-10-02 10:06:29 -0500

    feat: add button in activity search

--- app/src/main/java/com/example/studydesk/UploadDocumentsActivity.kt
+++ app/src/main/java/com/example/studydesk/UploadDocumentsActivity.kt
@@ -3,7 +3,7 @@ package com.example.studydesk
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class UploadDocumentsView : AppCompatActivity() {
+class UploadDocumentsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
